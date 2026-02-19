d24dbdac0d019fa811d52c6c58cbaf4ff78eef40
mkaychuks <sachufusi@gmail.com>
2026-02-19 19:51:29 +0100

    restarting the code base

--- src/main/java/ng/mkaychuks/authify/service/ProfileService.java
+++
@@ -1,9 +0,0 @@
-package ng.mkaychuks.authify.service;
-
-import ng.mkaychuks.authify.io.ProfileRequest;
-import ng.mkaychuks.authify.io.ProfileResponse;
-
-public interface ProfileService {
-
-    ProfileResponse createProfile(ProfileRequest request);
-}
